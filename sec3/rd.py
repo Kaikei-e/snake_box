49dabf90f7f1afd7e9ee42cd3bb86ba82da8762f
Kaikei-e <e_kaikei@protonmail.com>
2022-08-29 23:43:09 +0900

    Update: Implementing sec 3

--- sec3/rd.py
+++ sec3/rd.py
@@ -1,5 +1,18 @@
 from __future__ import print_function
 
-
 # import linux
+import tarfile
+import uuid
 
+import click
+import traceback
+import os
+import stat
+
+
+def _get_image_name(image_name, image_dir, image_suffix='tar'):
+    return os.path.join(image_dir, os.extsep.join([image_name, image_suffix]))
+
+
+def _get_container_path(container_id, container_dir, *subdir_names):
+    return os.path.join(container_dir, container_id, *subdir_names)
